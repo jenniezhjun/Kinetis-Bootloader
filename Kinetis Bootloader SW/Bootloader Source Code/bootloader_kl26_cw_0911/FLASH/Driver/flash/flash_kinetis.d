bf444036a0fd55335bd6e44de2d1476262ee2d93
jenniezhjun <b08277@freescale.com>
2015-09-18 15:55:56 +0800

    update Bootloader Source Code to 0911 version
    bootloader_kl26_cw_0911

---
+++ Kinetis Bootloader SW/Bootloader Source Code/bootloader_kl26_cw_0911/FLASH/Driver/flash/flash_kinetis.d
@@ -0,0 +1,46 @@
+Driver/flash/flash_kinetis.o: ../Driver/flash/flash_kinetis.c \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Driver/flash/flash_kinetis.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Sources/bootloader.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/bootcfg/FRDM_KL26Z_cfg.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/MKL26Z4.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Driver/flash/flash_kinetis.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Sources/bootloader.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/bootcfg/FRDM_KL26Z_cfg.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/MKL26Z4.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
---
+++ Kinetis Bootloader SW/Bootloader Source Code/bootloader_kl26_cw_0911/FLASH/Sources/bootloader.d
@@ -0,0 +1,55 @@
+Sources/bootloader.o: ../Sources/bootloader.c \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/derivative.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/MKL26Z4.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ ../Sources/bootloader.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/bootcfg/FRDM_KL26Z_cfg.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/rs232/rs232.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/flash/flash_kinetis.h \
+ C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Sources/bootloader.h
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/derivative.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Project_Headers/MKL26Z4.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+../Sources/bootloader.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/bootcfg/FRDM_KL26Z_cfg.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/rs232/rs232.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Driver/flash/flash_kinetis.h:
+
+C:/Users/b08277/Desktop/Bootloader20150911/bootloader_kl26_cw_0911/Sources/bootloader.h:
